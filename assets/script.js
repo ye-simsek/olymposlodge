9ea5a42fbbbf83ed4a397ac2e0f904f5dc6fdffb
Yunus Emre Şimşek <yunus@Yunuss-MacBook-Air-3.local>
2026-04-05 20:22:58 +0300

    Auto-save: 2026-04-05 20:22:58

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
+      smoothTouch: false,
     });
     function raf(time) {
       lenis.raf(time);
